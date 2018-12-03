37401463f69c6049c46f940d05a52ba54c444807
ant-mad <0.0.mad.ant@gmail.com>
2018-12-03 03:46:27 +0300

    Platform table

--- src/app/dashboard/dashboard/platforms/platforms.component.ts
+++ src/app/dashboard/dashboard/platforms/platforms.component.ts
@@ -1,42 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { AppService } from '../../../services/app.service';
+import { Component, OnInit } from "@angular/core";
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
+import { Observable, of } from "rxjs";
+import { map } from "rxjs/operators";
+import { AppService } from "../../../services/app.service";
 
 @Component({
-  selector: 'app-platforms',
-  templateUrl: './platforms.component.html',
-  styleUrls: ['./platforms.component.scss'],
+  selector: "app-platforms",
+  templateUrl: "./platforms.component.html",
+  styleUrls: ["./platforms.component.scss"]
 })
 export class PlatformsComponent implements OnInit {
   columnHeaders$: Observable<string[]> = of(DEFAULT_HEADERS);
-  platform$;
+  platforms$;
 
   constructor(
     private breakpointObserver: BreakpointObserver,
-    private appService: AppService,
+    private appService: AppService
   ) {}
 
   ngOnInit() {
     this.columnHeaders$ = this.breakpointObserver
       .observe(Breakpoints.XSmall)
       .pipe(map(result => (result.matches ? XSMALL_HEADERS : DEFAULT_HEADERS)));
-    this.platform$ = this.appService.recentTransactions$.pipe(
-      map(transactions => transactions.slice(0, 50)),
-    );
+    this.platforms$ = this.appService.getPlatformList();
   }
 }
 
 const DEFAULT_HEADERS = [
-  'platform_name',
-  'supply_share',
-  'scoring_round_time',
-  'users_count',
-  'emission_total',
-  'sr_time_last',
-  'cpu',
-  'net',
+  "platform_name",
+  "supply_share",
+  "scoring_round_time",
+  "users_count",
+  "emission_total",
+  "sr_time_last"
 ];
 
-const XSMALL_HEADERS = ['platform_name', 'supply_share'];
+const XSMALL_HEADERS = ["platform_name", "supply_share"];
